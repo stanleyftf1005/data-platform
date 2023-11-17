a6bee14d980264ecdaf154ae85cf38dc51dee132
Stanley Fung <stanly19981005@gmail.com>
2023-11-17 19:28:27 +0800

    revert to clerk

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,12 +11,11 @@ export default function Layout({
   children: React.ReactNode
 }) {
 
-  /*
   const {userId} = auth()
 
   if(!userId) {
     return redirect('/')
-  }*/
+  }
 
   return (
     <>
