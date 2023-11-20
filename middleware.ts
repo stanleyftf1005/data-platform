9cfaadf19e9f3480ee3e39715f5518068b5a193e
Stanley Fung <stanly19981005@gmail.com>
2023-11-20 16:38:05 +0800

    debud mode authmiddleware

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,6 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
     debug: true,
-    signInUrl: 'https://accounts.rnx.app/sign-in',
-    frontendApi: 'https://accounts.rnx.app/sign-in',
 
 });
  
